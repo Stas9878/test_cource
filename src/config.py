435a613565f58d870272aba6f8bfe01f9a583adb
Popov Stas <boss.staspopov@mail.ru>
2024-04-09 11:56:33 +0300

    Добавил файл env

--- src/config.py
+++ src/config.py
@@ -1,9 +1,21 @@
-from os import getenv
 from pydantic_settings import BaseSettings
+from dotenv import load_dotenv
+import os
+
+
+load_dotenv()
+
+DB_USER = os.environ.get('DB_USER')
+DB_NAME = os.environ.get('DB_NAME')
+DB_HOST = os.environ.get('DB_HOST')
+DB_PORT = os.environ.get('DB_PORT')
+DB_PASSWORD = os.environ.get('DB_PASSWORD')
+
 
 class Settings(BaseSettings):
-    db_url: str = f'postgresql+asyncpg://home:123@localhost:5432/shop_fastapi_test_cource'
+    db_url: str = f'postgresql+asyncpg://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}'
     db_echo: bool = True
+    
 
 settings = Settings()
 
