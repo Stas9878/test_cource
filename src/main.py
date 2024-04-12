2ded0e3940e5502f09c60b6663a6d4f882bd05b5
Popov Stas <boss.staspopov@mail.ru>
2024-04-12 21:58:18 +0300

    Убрал возможность создания всех таблиц при старте приложения

--- src/main.py
+++ src/main.py
@@ -1,7 +1,5 @@
 from contextlib import asynccontextmanager
 from fastapi import FastAPI
-from database import Base
-from db_helper import db_helper
 from items.router import router as items_router
 from users.router import router as users_router
 from products.router import router as products_router
@@ -10,8 +8,6 @@ app = FastAPI()
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    async with db_helper.engine.begin() as conn:
-        await conn.run_sync(Base.metadata.create_all)
     yield
     
 
