ab1f565d8130cce118b9f0e83c160419b1366e13
Popov Stas <boss.staspopov@mail.ru>
2024-04-18 16:32:03 +0300

    Добавлены функции для кидорования декодирования jwt

--- src/config.py
+++ src/config.py
@@ -1,3 +1,4 @@
+from pathlib import Path
 from pydantic_settings import BaseSettings
 from dotenv import load_dotenv
 from pydantic import BaseModel
@@ -13,14 +14,22 @@ DB_PORT = os.environ.get('DB_PORT')
 DB_PASSWORD = os.environ.get('DB_PASSWORD')
 
 
+BASE_DIR = Path(__file__).parent.parent
+
+
 class DBSettings(BaseModel):
     url: str = f'postgresql+asyncpg://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}'
     echo: bool = False
 
 
+class AuthJWT(BaseModel):
+    private_key_path: Path = BASE_DIR / 'certificates' / 'jwt-private.pem'
+    public_key_path: Path = BASE_DIR / 'certificates' / 'jwt-public.pem'
+    algorithm: str = 'RS256'
+
 class Settings(BaseSettings):
     db: DBSettings = DBSettings()
-    
+    auth_jwt: AuthJWT = AuthJWT()
 
 settings = Settings()
 
--- src/users/schemas.py
+++ src/users/schemas.py
@@ -1,7 +1,16 @@
-from pydantic import BaseModel, EmailStr
+from pydantic import BaseModel, EmailStr, ConfigDict
 from typing import Annotated
 from annotated_types import MinLen, MaxLen
 
 class CreateUser(BaseModel):
     username: Annotated[str, MinLen(3), MaxLen(20)]
     email: EmailStr
+
+
+class UserSchema(BaseModel):
+    model_config = ConfigDict(strict=True)
+    
+    username: str
+    password: bytes
+    email: EmailStr | None = None
+    active: bool = True
